efb5bf9071fdec9376ca59654da5d4f3eaddb541
Mark Sandan <msandan@utexas.edu>
2015-03-18 16:39:34 -0500

    add priority_q impl

---
+++ queues/priority_q/priority_q.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "../../headers/priority_q.h"
+
+//int based priority queue based on min heap
+void pq_insert(pq *priority_q, int x){
+
+
+}
+
+int get_min(pq* priority_q){
+ return priority_q->H->heap[1]; //1 idxed array
+}
+
+int extract_min(pq* priority_q){
+ if (priority_q->H->size < 1) abort(); //no elements to return
+
+ int result = get_min(priority_q);
+
+ //replace top with a leaf
+ priority_q->H->heap[1] = priority_q->H->heap[priority_q->H->size];
+ priority_q->H->size-=1;
+ min_heapify(priority_q->H, 1);
+ return result;
+}
